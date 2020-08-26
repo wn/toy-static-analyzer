e4b8e23b555704c5b9a33c57d377ff007972fb0e
Ang Wei Neng <weineng.a@gmail.com>
2020-08-26 16:22:49 +0800

    Add PKB scaffold

--- Team00/Code00/src/spa/src/PKB.cpp
+++ Team00/Code00/src/spa/src/PKB.cpp
@@ -1,9 +1,9 @@
+#include <Lexer.h>
+#include <fstream>
 #include <iostream>
 #include <stdio.h>
 #include <string>
 #include <vector>
-
-using namespace std;
 
 #include "PKB.h"
 #include "TNode.h"
@@ -14,4 +14,32 @@ int PKB::setProcToAST(PROC p, TNode* r) {
 
 TNode* PKB::getRootAST(PROC p) {
     return nullptr;
+}
+
+STATEMENTS PKB::getPreFollows(STATEMENT s) {
+    return STATEMENTS();
+}
+
+STATEMENTS PKB::getPostFollows(STATEMENT s) {
+    return STATEMENTS();
+}
+
+bool PKB::isFollow(STATEMENT s1, STATEMENT s2) {
+    return false;
+}
+
+STATEMENTS PKB::getParents(STATEMENT s) {
+    return STATEMENTS();
+}
+
+STATEMENTS PKB::getChildren(STATEMENT s) {
+    return STATEMENTS();
+}
+
+bool PKB::isParent(STATEMENT s1, STATEMENT s2) {
+    return false;
+}
+
+STATEMENTS PKB::getBlockStatementsNumber(STATEMENT s) {
+    return STATEMENTS();
 }
--- Team00/Code00/src/spa/src/PKB.h
+++ Team00/Code00/src/spa/src/PKB.h
@@ -5,8 +5,9 @@
 #include <string>
 #include <vector>
 
-using namespace std;
 typedef short PROC;
+typedef int STATEMENT;
+typedef std::vector<STATEMENT> STATEMENTS;
 
 class TNode;
 
@@ -17,4 +18,21 @@ class PKB {
     static VarTable* varTable;
     static int setProcToAST(PROC p, TNode* r);
     static TNode* getRootAST(PROC p);
+
+    /* API for follows */
+    // Given s, return all s' such that follow*(s', s).
+    STATEMENTS getPreFollows(STATEMENT s);
+    // Given s, return all s' such that follow*(s, s').
+    STATEMENTS getPostFollows(STATEMENT s);
+    bool isFollow(STATEMENT s1, STATEMENT s2);
+
+    /* API for parents */
+    STATEMENTS getParents(STATEMENT s);
+    STATEMENTS getChildren(STATEMENT s);
+    // Check if s1 is a parent of s2
+    bool isParent(STATEMENT s1, STATEMENT s2);
+
+  private:
+    // get all statements in the same block as s.
+    STATEMENTS getBlockStatementsNumber(STATEMENT s);
 };
