995a790a8fee8af4b4dc553df196988a10e21dd5
EleenYang <34708166+EleenYang@users.noreply.github.com>
2020-09-15 13:32:30 +0800

    Add Header File for Query (#109)
    * add header file of Query
    
    * WIP
    
    * add header file for Query
    
    * add query header file
    
    * fix typo and set all members and constructor in struct Query as public.
    
    * remove redundant relation types
    
    * change pattern clause format to <PatternType, synonym, arg1, arg2>
    
    * remove 'valid' from Query; change returnValues to synonymsToReturn
    
    * remove pattern type from pattern clause
    
    * remove unused enum PatternType

---
+++ Team00/Code00/src/spa/src/Query.h
@@ -0,0 +1,44 @@
+#ifndef QUERY_H
+#define QUERY_H
+
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+namespace qpbackend {
+enum EntityType {
+    STMT,
+    READ,
+    PRINT,
+    CALL,
+    WHILE,
+    IF,
+    ASSIGN,
+    VARIABLE,
+    CONSTANT,
+    PROCEDURE
+};
+
+enum RelationType {
+    FOLLOWS,
+    FOLLOWST,
+    PARENT,
+    PARENTT,
+    USES,
+    MODIFIES
+};
+
+struct Query {
+    public:
+        std::unordered_map<std::string, EntityType> declarationMap;
+        std::vector<std::string> synonymsToReturn;
+        std::vector<std::tuple<RelationType, std::string, std::string>> suchThatClauses;
+        std::vector<std::tuple<std::string, std::string, std::string>> patternClauses;
+
+        explicit Query() : declarationMap(), suchThatClauses(), patternClauses(),
+          synonymsToReturn(){};
+};
+
+} // namespace qpbackend
+
+#endif // QUERY_H
