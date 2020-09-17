fa673ecbe509608e8321681d4888f69cd01bf03b
EleenYang <34708166+EleenYang@users.noreply.github.com>
2020-09-17 13:43:19 +0800

    Query Evaluator Header File Design (#123)
    * add header file of Query
    
    * WIP
    
    * add header file for Query
    
    * add query header file
    
    * fix typo and set all members and constructor in struct Query as public.
    
    * remove redundant relation types
    
    * change pattern clause format to <PatternType, synonym, arg1, arg2>
    
    * remove 'valid' from Query; change returnValues to synonymsToReturn
    
    * remove pattern type from pattern clause
    
    * add qe related header files
    
    * add header file annotation
    
    * clang-format Query.h
    
    * clang-format QueryEvaluator.h
    
    * move helper methods to a separete file QEHelper.h
    
    * update helper.h
    
    * remove unused enum PatternType
    
    * change std::string const& to const std::string&
    
    * add new line
    
    * clang-format
    
    * clang-format Query.h
    
    * remove clear method, instead throwing error when evaluateQuery is called twice
    
    * change tuple argument to const reference to avoid copying
    
    * change all vector/tuple arguments to const reference type
    
    * change string in tuple back to value type
    
    * change reference to const reference
    
    * update method description
    
    * clang format

---
+++ Team24/Code24/src/spa/src/QEHelper.h
@@ -0,0 +1,26 @@
+#ifndef QEHELPER_H
+#define QEHELPER_H
+
+#include "PKB.h"
+#include "Query.h"
+#include <algorithm>
+#include <string>
+#include <utility>
+#include <vector>
+
+namespace qpbackend {
+namespace queryevaluator {
+// methods to check string argument
+bool isWildCard(const std::string& str);
+bool isStmt(const std::string& str);
+bool isPosInt(const std::string& str);
+
+// helper functions for vector operations
+template <typename T> std::vector<std::string> vector_str_cast(const std::vector<T>& v);
+template <typename T> bool vector_contain(const std::vector<T>& v, T arg);
+template <typename T>
+std::vector<T> vector_intersection(const std::vector<T>& lst1, const std::vector<T>& lst2);
+} // namespace queryevaluator
+} // namespace qpbackend
+
+#endif // QEHELPER_H
