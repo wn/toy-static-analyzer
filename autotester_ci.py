a020096aa6bba723be26cd503fe5c458d8f5e369
jeffkwoh <23180853+jeffkwoh@users.noreply.github.com>
2020-09-22 02:17:57 +0800

    feat/autotester_ci.py: check and raise failed test cases (#133)
    Checks the outcome of autotester run via the presence of failure tags in the outputted XML file.
    
    Review from `Check if autotester test fails` onwards.
    
    Dependent on: #132 
    Fixes: #70
    TODOs: #135 #134

--- autotester_ci.py
+++ autotester_ci.py
@@ -10,15 +10,20 @@
 
 import os
 import sys
-from collections import namedtuple
+from collections import namedtuple, Counter
 
 # Edit this to change what source and query file pairs to run.
-SIMPLE_SOURCE_QPL_QUERY_PAIRS = [('trivial_source', 'Sample_queries')]
+SIMPLE_SOURCE_QPL_QUERY_PAIRS = [('trivial_source', 'trivial_queries')]
 # Named tuple(s)
 AutotesterParameters = namedtuple('AutotesterParameters', ['source_filepath', 'query_filepath', 'output_filepath'])
 # Dev constants
 WINDOWS_OS_NAME = 'nt'
 SUCCESS_EXIT_CODE = 0
+# Output XML constants
+# Whenever a test fails, the output xml will enclose the reason for failure in between `<failed> </failed>` tags.
+FAILURE_CLOSING_TAG = '</failed>'
+# Whenever a test passes, the output xml will have a `<passed/>` tag.
+SUCCESS_TAG = '<passed/>'
 
 
 def log(x):
@@ -47,8 +52,24 @@ def execute_autotester(autotester_filepath, autotester_parameters):
     exit_code = os.system(command)
     if exit_code != SUCCESS_EXIT_CODE:
         raise RuntimeError(f'Autotester execution failed with exit code: {exit_code}')
-    # TODO(https://github.com/nus-cs3203/team24-cp-spa-20s1/issues/70):
-    # Read and check output of xml file here
+    check_output_xml(autotester_parameters.output_filepath)
+
+
+def check_output_xml(output_xml):
+    with open(output_xml) as f:
+        output = f.read()
+        # Analyze chunks of the output file.
+        text_chunks = Counter(output.split())
+        passed_test_cases = text_chunks[SUCCESS_TAG]
+        failed_test_cases = text_chunks[FAILURE_CLOSING_TAG]
+        # TODO(https://github.com/nus-cs3203/team24-cp-spa-20s1/issues/135):
+        # Provide more descriptive test summary on test cases failed and reason for failure.
+        test_summary = f'Test passed:{passed_test_cases}\nTest failed:{failed_test_cases}'
+        if failed_test_cases > 0:
+            raise RuntimeError(f'Some test cases failed.\n{test_summary}')
+        # TODO(https://github.com/nus-cs3203/team24-cp-spa-20s1/issues/134):
+        # Check if any of the queries exceed the 5s limit.
+        log(test_summary)
 
 
 def main():
